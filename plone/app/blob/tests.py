182b603326e96719fe8627bd023f62e9b38b1ac8
Andreas Zeidler <witsch@plone.org>
2007-11-17 22:29:14 +0000

    #6805:  added test [http://svn.zope.org/Zope/branches/2.9-with-ZODB3.8/?rev=73132&view=rev originally introduced] in the zope 2.9 branch with zodb 3.8
    svn path=/plone.app.blob/trunk/; revision=18032

--- plone/app/blob/tests.py
+++ plone/app/blob/tests.py
@@ -1,4 +1,4 @@
-from unittest import TestSuite
+from unittest import TestSuite, makeSuite
 from Testing.ZopeTestCase import installPackage, ZopeDocFileSuite
 from Products.Five import zcml
 from Products.Five import fiveconfigure
@@ -21,8 +21,38 @@ PloneTestCase.setupPloneSite(products=(
 ))
 
 
+test_environment = {
+    'CONTENT_TYPE': 'multipart/form-data; boundary=12345',
+    'REQUEST_METHOD': 'POST',
+    'SERVER_NAME': 'localhost',
+    'SERVER_PORT': '80',
+}
+
+largefile_data = '''
+--12345
+Content-Disposition: form-data; name="file"; filename="file"
+Content-Type: application/octet-stream
+
+test %s
+
+''' % ('test' * 1000)
+
+
+class BlobTestCase(PloneTestCase.PloneTestCase):
+
+    def testFileName(self):
+        """ checks fileupload object supports the filename """
+        from StringIO import StringIO
+        from ZPublisher.HTTPRequest import HTTPRequest
+        req = HTTPRequest(StringIO(largefile_data), test_environment.copy(), None)
+        req.processInputs()
+        f = req.form.get('file')
+        self.assert_(f.name)
+
+
 def test_suite():
     return TestSuite((
+        makeSuite(BlobTestCase),
         ZopeDocFileSuite(
            'README.txt', package='plone.app.blob',
            test_class=PloneTestCase.FunctionalTestCase),
