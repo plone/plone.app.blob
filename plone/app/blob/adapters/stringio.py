3e6c85266960c33e42bd5808f01c6a7b1909fe5c
Andreas Zeidler <witsch@plone.org>
2007-12-04 00:58:30 +0000

    #6805:  added adapter for initializing the blob field from [http://docs.python.org/lib/module-StringIO.html StringIO] instances;  this fixes more tests from ATCT's [http://dev.plone.org/collective/browser/ATContentTypes/branches/1.2/tests/test_atfile.py?rev=49358 test_atfile.py], but might also come in handy later on...
    svn path=/plone.app.blob/trunk/; revision=18270

---
+++ plone/app/blob/adapters/stringio.py
@@ -0,0 +1,28 @@
+from zope.interface import implements
+from zope.component import adapts
+
+from StringIO import StringIO
+
+from plone.app.blob.interfaces import IBlobbable
+
+
+class BlobbableStringIO(object):
+    """ adapter for StringIO instance to work with blobs """
+    implements(IBlobbable)
+    adapts(StringIO)
+
+    def __init__(self, context):
+        self.context = context
+
+    def feed(self, blob):
+        """ see interface ... """
+        self.context.getvalue()
+
+    def filename(self):
+        """ see interface ... """
+        return None
+
+    def mimetype(self):
+        """ see interface ... """
+        return 'text/plain'
+
