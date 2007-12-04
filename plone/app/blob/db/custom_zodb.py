1650895c240c40f91f8755d47849beb7e069bc90
Andreas Zeidler <witsch@plone.org>
2007-12-04 21:57:10 +0000

    #6805:  output a message when setting up the custom zodb to make it easier to spot problems when running tests...
    svn path=/plone.app.blob/trunk/; revision=18284

--- plone/app/blob/db/custom_zodb.py
+++ plone/app/blob/db/custom_zodb.py
@@ -3,4 +3,5 @@ from ZODB.DemoStorage import DemoStorage
 from ZODB.blob import BlobStorage
 from tempfile import mkdtemp
 
+print 'Setting up blob-aware ZODB storage ...'
 Storage = BlobStorage(mkdtemp(), DemoStorage(quota=(1<<20)))
