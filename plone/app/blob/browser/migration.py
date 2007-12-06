d96fd170d1ac4907120a1b811c036dae48d216ea
Andreas Zeidler <witsch@plone.org>
2007-12-06 16:33:08 +0000

    #6805:  restructured `ATFile` migration and gave it something resembling a UI :)
    svn path=/plone.app.blob/trunk/; revision=18350

--- plone/app/blob/browser/migration.py
+++ plone/app/blob/browser/migration.py
@@ -1,14 +1,39 @@
+from Acquisition import aq_inner
+from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone import PloneMessageFactory as _
 from Products.Five import BrowserView
-from StringIO import StringIO
+from Products.statusmessages.interfaces import IStatusMessage
 
+from plone.app.blob.migrations import haveContentMigrations
 from plone.app.blob.migrations import migrateATFiles
+from plone.app.blob.migrations import getATFilesMigrationWalker
 
 
 class MigrationView(BrowserView):
 
-    def migrateFiles(self):
-        out = StringIO()
-        print >> out, "starting ATFile migrations..."
-        print >> out, migrateATFiles(self)
-        return out.getvalue()
+    def __call__(self):
+        context = aq_inner(self.context)
+        request = aq_inner(self.request)
+        options = {}
+        clicked = request.form.has_key
+        if not haveContentMigrations:
+            msg = _(u'Please install contentmigrations to be able to migrate to blobs.')
+            IStatusMessage(request).addStatusMessage(msg, type='warning')
+            options = { 'notinstalled': 42 }
+        elif clicked('migrate'):
+            output = migrateATFiles(self)
+            count = len(output.split('\n')) - 1
+            msg = _(u'blob_migration_info',
+                default=u'Blob migration performed for ${count} item(s).',
+                mapping={'count': count})
+            IStatusMessage(request).addStatusMessage(msg, type='info')
+            options = { 'count': count, 'output': output }
+        elif clicked('cancel'):
+            msg = _(u'Blob migration cancelled.')
+            IStatusMessage(request).addStatusMessage(msg, type='info')
+            request.RESPONSE.redirect(getToolByName(context, 'portal_url')())
+        else:
+            walker = getATFilesMigrationWalker(self)
+            options = { 'available': len(list(walker.walk())) }
+        return self.index(**options)
 
--- plone/app/blob/migrations.py
+++ plone/app/blob/migrations.py
@@ -9,28 +9,30 @@ from Products.CMFCore.utils import getToolByName
 from transaction import savepoint
 
 
+class ATFileToBlobMigrator(ATItemMigrator):
+    src_portal_type = 'File'
+    src_meta_type = 'ATFile'
+    dst_portal_type = 'Blob'
+    dst_meta_type = 'ATBlob'
+
+    # migrate all fields except 'file', which needs special handling...
+    fields_map = {
+        'file': None,
+    }
+
+    def migrate_data(self):
+        self.new.getField('file').getMutator(self.new)(self.old)
+
+
+def getATFilesMigrationWalker(self):
+    """ set up walker for migrating ATFile instances """
+    portal = getToolByName(self, 'portal_url').getPortalObject()
+    return CustomQueryWalker(portal, ATFileToBlobMigrator, query = {})
+
+
 def migrateATFiles(self):
     """ migrate ATFile instances """
-
-    if not haveContentMigrations:
-        return 'WARNING: Please install contentmigrations to be able to migrate ATFiles.'
-
-    class ATFileToBlobMigrator(ATItemMigrator):
-        src_portal_type = 'File'
-        src_meta_type = 'ATFile'
-        dst_portal_type = 'Blob'
-        dst_meta_type = 'ATBlob'
-
-        # migrate all fields except 'file', which needs special handling...
-        fields_map = {
-            'file': None,
-        }
-
-        def migrate_data(self):
-            self.new.getField('file').getMutator(self.new)(self.old)
-
-    portal = getToolByName(self, 'portal_url').getPortalObject()
-    walker = CustomQueryWalker(portal, ATFileToBlobMigrator, query = {})
+    walker = getATFilesMigrationWalker(self)
     savepoint(optimistic=True)
     walker.go()
     return walker.getOutput()
