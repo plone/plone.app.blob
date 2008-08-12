34ed54b58927050e7bf5672a30d6a9936452ea18
Andreas Zeidler <witsch@plone.org>
2008-08-12 22:27:48 +0000

    factor out test helper method
    svn path=/plone.app.blob/trunk/; revision=22137

--- plone/app/blob/tests/test_integration.py
+++ plone/app/blob/tests/test_integration.py
@@ -2,40 +2,16 @@ from plone.app.blob.tests.base import BlobTestCase      # import first!
 
 import os.path
 from unittest import defaultTestLoader
-from ZPublisher.HTTPRequest import HTTPRequest
 
 from plone.app.blob.utils import guessMimetype
+from plone.app.blob.tests.utils import makeFileUpload
 
 from StringIO import StringIO
 from base64 import decodestring
 
 
-test_environment = {
-    'CONTENT_TYPE': 'multipart/form-data; boundary=12345',
-    'REQUEST_METHOD': 'POST',
-    'SERVER_NAME': 'localhost',
-    'SERVER_PORT': '80',
-}
-
 largefile_data = ('test' * 2048)
 
-upload_request = '''
---12345
-Content-Disposition: form-data; name="file"; filename="%s"
-Content-Type: application/octet-stream
-Content-Length: %d
-
-%s
-
-'''
-
-def makeFileUpload(data, filename):
-    request_data = upload_request % (filename, len(data), data)
-    req = HTTPRequest(StringIO(request_data),
-                      test_environment.copy(),
-                      None)
-    req.processInputs()
-    return req.form.get('file')
 
 class IntegrationTests(BlobTestCase):
 
---
+++ plone/app/blob/tests/utils.py
@@ -0,0 +1,29 @@
+from ZPublisher.HTTPRequest import HTTPRequest
+from StringIO import StringIO
+
+
+test_environment = {
+    'CONTENT_TYPE': 'multipart/form-data; boundary=12345',
+    'REQUEST_METHOD': 'POST',
+    'SERVER_NAME': 'localhost',
+    'SERVER_PORT': '80',
+}
+
+upload_request = '''
+--12345
+Content-Disposition: form-data; name="file"; filename="%s"
+Content-Type: application/octet-stream
+Content-Length: %d
+
+%s
+
+'''
+
+def makeFileUpload(data, filename):
+    request_data = upload_request % (filename, len(data), data)
+    req = HTTPRequest(StringIO(request_data),
+                      test_environment.copy(),
+                      None)
+    req.processInputs()
+    return req.form.get('file')
+
