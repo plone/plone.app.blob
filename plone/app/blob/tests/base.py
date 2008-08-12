2ba3fa171f8fca7e50cd997e857ed77cdc297bc5
Andreas Zeidler <witsch@plone.org>
2008-08-12 22:28:51 +0000

    use layer to load additional gs profile in order to test the replacement types for `ATFile` (and later also `ATImage`)
    svn path=/plone.app.blob/trunk/; revision=22141

--- plone/app/blob/tests/base.py
+++ plone/app/blob/tests/base.py
@@ -1,11 +1,13 @@
 from plone.app.blob.tests import db # needs to be imported first to set up ZODB
 db  # make pyflakes happy...
 
-from Testing.ZopeTestCase import installPackage
+from Testing.ZopeTestCase import installPackage, app, close
 from Products.Five import zcml
 from Products.Five import fiveconfigure
+from Products.CMFCore.utils import getToolByName
 from Products.PloneTestCase import PloneTestCase
-from Products.PloneTestCase.layer import onsetup
+from Products.PloneTestCase.layer import PloneSite, onsetup
+from transaction import commit
 
 
 @onsetup
@@ -29,4 +31,33 @@ class BlobTestCase(PloneTestCase.PloneTestCase):
 
 class BlobFunctionalTestCase(PloneTestCase.FunctionalTestCase):
     """ base class for functional tests """
+
+
+class BlobReplacementLayer(PloneSite):
+    """ layer for integration tests using replacement types """
+
+    @classmethod
+    def setUp(cls):
+        root = app()
+        portal = root.plone
+        # import replacement profiles
+        profile = 'profile-plone.app.blob:atfile-testing'
+        tool = getToolByName(portal, 'portal_setup')
+        tool.runAllImportStepsFromProfile(profile, purge_old=False)
+        # make sure it's loaded...
+        types = getToolByName(portal, 'portal_types')
+        assert types.getTypeInfo('File').product == 'plone.app.blob'
+        # and commit the changes
+        commit()
+        close(root)
+
+    @classmethod
+    def tearDown(cls):
+        pass
+
+
+class ReplacementTestCase(PloneTestCase.PloneTestCase):
+    """ base class for integration tests using replacement types """
+
+    layer = BlobReplacementLayer
 
