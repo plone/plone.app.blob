0efb1a7503520da03aa460bc5851eb58964c6e86
Stefan H. Holek <stefan@epy.co.at>
2008-12-31 20:48:46 +0000

    "The user of mkdtemp() is responsible for deleting the temporary directory and its contents when done with it."
    svn path=/plone.app.blob/trunk/; revision=24022

--- plone/app/blob/tests/db/__init__.py
+++ plone/app/blob/tests/db/__init__.py
@@ -11,13 +11,17 @@ from Testing.ZopeTestCase import ZopeLite
 from ZODB.DemoStorage import DemoStorage
 from ZODB.blob import BlobStorage
 from tempfile import mkdtemp
+from shutil import rmtree
+from atexit import register
 
 def sandbox(base=None):
     """ returns a sandbox copy of the base ZODB """
     if base is None:
         base = ZopeLite.Zope2.DB
+    blobdir = mkdtemp()
+    register(rmtree, blobdir)
     storage = DemoStorage(base=base._storage)
-    storage = BlobStorage(mkdtemp(), storage)
+    storage = BlobStorage(blobdir, storage)
     return ZopeLite.ZODB.DB(storage)
 
 ZopeLite.sandbox = sandbox
--- plone/app/blob/tests/db/custom_zodb.py
+++ plone/app/blob/tests/db/custom_zodb.py
@@ -2,6 +2,10 @@ import ZODB; ZODB   # make pyflakes happy...
 from ZODB.DemoStorage import DemoStorage
 from ZODB.blob import BlobStorage
 from tempfile import mkdtemp
+from shutil import rmtree
+from atexit import register
 
 print 'Setting up blob-aware ZODB storage ...'
-Storage = BlobStorage(mkdtemp(), DemoStorage())
+blobdir = mkdtemp()
+register(rmtree, blobdir)
+Storage = BlobStorage(blobdir, DemoStorage())
