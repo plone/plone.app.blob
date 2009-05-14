39f0dc0d7acd81b89a43b403e4c24b2607a61141
Andreas Zeidler <witsch@plone.org>
2009-05-14 13:32:27 +0000

    fix test setup after r26916
    svn path=/plone.app.blob/trunk/; revision=26922

--- src/plone/app/blob/tests/base.py
+++ src/plone/app/blob/tests/base.py
@@ -24,6 +24,7 @@ def setupPackage():
     zcml.load_config('configure.zcml', plone.app.imaging)
     fiveconfigure.debug_mode = False
     installPackage('plone.app.blob', quiet=True)
+    installPackage('plone.app.imaging', quiet=True)
 
 installProduct('LinguaPlone', quiet=True)
 setupPackage()
