116ad8dd7b2a6d66cacce31cc3da08686d00fd34
Andreas Zeidler <witsch@plone.org>
2009-05-15 00:45:37 +0000

    drop the [http://pypi.python.org/pypi/collective.recipe.omelette/ omelette] in favour of [http://pypi.python.org/pypi/mr.developer/ mr.developer] as a tool to allow applying the necessary patches for running bbb-tests
    svn path=/plone.app.blob/trunk/; revision=26940

--- test-compatibility.sh
+++ test-compatibility.sh
@@ -6,4 +6,5 @@ cat test-patches/* | patch -Nsp0
 bin/instance test --nowarn -s Products.CMFPlone
 bin/instance test --nowarn -s Products.ATContentTypes
 cat test-patches/* | patch -Rsp0
+find extras/ -name \*.orig | xargs rm
 bin/buildout -Nq
