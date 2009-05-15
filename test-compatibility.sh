3da3336ae614106e506417f9464bee580b52df39
Andreas Zeidler <witsch@plone.org>
2009-05-15 00:45:14 +0000

    actually, merely adding [http://pypi.python.org/pypi/Products.LinguaPlone/ LinguaPlone] to the buildout and thereby loading its monkey-patches already causes test failures when running the bbb-tests.  to remedy a separate buildout variant is added, which simply removes the [http://pypi.python.org/pypi/Products.LinguaPlone/ LinguaPlone] egg
    svn path=/plone.app.blob/trunk/; revision=26939

--- test-compatibility.sh
+++ test-compatibility.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 cd $( dirname $0 )
+bin/buildout -Nqc test-bbb.cfg
 cat test-patches/* | patch -Nsp0
 bin/instance test --nowarn -s Products.CMFPlone
 bin/instance test --nowarn -s Products.ATContentTypes
 cat test-patches/* | patch -Rsp0
+bin/buildout -Nq
