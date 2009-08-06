94def08a97593f5384a36c7afa075af29435b4e8
Andreas Zeidler <witsch@plone.org>
2009-08-06 11:14:05 +0000

    let the buildout itself do the patching required to run the bbb tests.  that way it can be more easily added to a buildbot or other automatic test runs
    svn path=/plone.app.blob/trunk/; revision=28388

--- test-compatibility.sh
+++ test-compatibility.sh
@@ -2,9 +2,8 @@
 
 cd $( dirname $0 )
 bin/buildout -Nqc test-bbb.cfg
-cat test-patches/* | patch -Nsp0
 bin/instance test --nowarn -s Products.CMFPlone
 bin/instance test --nowarn -s Products.ATContentTypes
-cat test-patches/* | patch -Rsp0
-find extras/ -name \*.orig | xargs rm
+cat test-patches/* | patch -REsp0 --no-backup-if-mismatch
+find extras/ -name \*.orig -o -name \*.rej | xargs rm
 bin/buildout -Nq
