83b268ed92773b688ff83a4605519ce0e2ab61d2
Andreas Zeidler <witsch@plone.org>
2009-08-09 20:55:43 +0000

    add bbb tests for [http://pypi.python.org/pypi/plone.app.linkintegrity/ plone.app.linkintegrity] to make sure it's still working with [http://pypi.python.org/pypi/plone.app.blob plone.app.blob] enabled
    svn path=/plone.app.blob/trunk/; revision=28588

--- test-compatibility.sh
+++ test-compatibility.sh
@@ -4,6 +4,7 @@ cd $( dirname $0 )
 bin/buildout -Nqc test-bbb.cfg
 bin/instance test --nowarn -s Products.CMFPlone
 bin/instance test --nowarn -s Products.ATContentTypes
+bin/instance test --nowarn -s plone.app.linkintegrity
 cat test-patches/* | patch -REsp0 --no-backup-if-mismatch
 find extras/ -name \*.orig -o -name \*.rej | xargs rm
 bin/buildout -Nq
