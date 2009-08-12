1c0d4a7f1464e3cea96ab56f2d65db24f3f4d3a1
Andreas Zeidler <witsch@plone.org>
2009-08-12 22:27:52 +0000

    now that [http://blobot.zitc.de/builders/bbb-3.1%20slave/builds/5 all bbb tests turned green], let's change that again and enable "image support"...
    svn path=/plone.app.blob/trunk/; revision=28749

--- src/plone/app/blob/tests/bbb.py
+++ src/plone/app/blob/tests/bbb.py
@@ -3,11 +3,11 @@
 
 from Testing.ZopeTestCase import installProduct
 from plone.app.blob.tests import db     # set up a blob-aware zodb first
-from plone.app.blob.tests.layer import BlobFileReplacementLayer
+from plone.app.blob.tests.layer import BlobReplacementLayer
 
 
 installProduct('GenericSetup', quiet=True)
 installProduct('Marshall', quiet=True)
 
-plone = BlobFileReplacementLayer
+plone = BlobReplacementLayer
 db  # make pyflakes happy
