c4d2dfd5f8854500997301486e4d5a3aa6a4524d
Andreas Zeidler <witsch@plone.org>
2009-11-06 15:50:55 +0000

    only point the test setup to our private `custom_zodb.py` we have to, i.e. for plone 3.x
    svn path=/plone.app.blob/trunk/; revision=31157

--- src/plone/app/blob/tests/db/__init__.py
+++ src/plone/app/blob/tests/db/__init__.py
@@ -1,15 +1,19 @@
+from ZODB.interfaces import IBlobStorage
+from ZODB.DemoStorage import DemoStorage
 from os.path import dirname
 
-import Testing; Testing # make pyflakes happy...
-import App.config
-cfg = App.config.getConfiguration()
-cfg.testinghome = dirname(__file__)
+
+# point the test setup to our private `custom_zodb.py`, but only if we
+# have to, i.e. for plone 3.x
+if not IBlobStorage.providedBy(DemoStorage()):
+    import Testing; Testing # make pyflakes happy...
+    import App.config
+    cfg = App.config.getConfiguration()
+    cfg.testinghome = dirname(__file__)
 
 
 # ZopeLite uses DemoStorage directly, so it needs monkey-patching... :(
 from Testing.ZopeTestCase import ZopeLite
-from ZODB.interfaces import IBlobStorage
-from ZODB.DemoStorage import DemoStorage
 from ZODB.blob import BlobStorage
 from tempfile import mkdtemp
 from shutil import rmtree
