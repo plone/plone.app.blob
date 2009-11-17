0800bb4ddc03a0858abb9e82ac030db3ecc7626d
Andreas Zeidler <witsch@plone.org>
2009-11-17 00:49:36 +0000

    factor out helpers for setting up proper download headers and range support so they can be cleaned up and tested more easily (later)
    svn path=/plone.app.blob/trunk/; revision=31717

---
+++ src/plone/app/blob/download.py
@@ -0,0 +1,82 @@
+from DateTime.DateTime import DateTime
+from ZPublisher.HTTPRangeSupport import parseRange
+
+
+def handleIfModifiedSince(instance, REQUEST, RESPONSE):
+    # HTTP If-Modified-Since header handling: return True if
+    # we can handle this request by returning a 304 response
+    header = REQUEST.get_header('If-Modified-Since', None)
+    if header is not None:
+        header = header.split(';')[0]
+        # Some proxies seem to send invalid date strings for this
+        # header. If the date string is not valid, we ignore it
+        # rather than raise an error to be generally consistent
+        # with common servers such as Apache (which can usually
+        # understand the screwy date string as a lucky side effect
+        # of the way they parse it).
+        # This happens to be what RFC2616 tells us to do in the face of an
+        # invalid date.
+        try:
+            mod_since = long(DateTime(header).timeTime())
+        except:
+            mod_since = None
+        if mod_since is not None:
+            if instance._p_mtime:
+                last_mod = long(instance._p_mtime)
+            else:
+                last_mod = long(0)
+            if last_mod > 0 and last_mod <= mod_since:
+                RESPONSE.setStatus(304)
+                return True
+
+
+def handleRequestRange(instance, blob, REQUEST, RESPONSE):
+    # check if we have a range in the request
+    ranges = None
+    range = REQUEST.get_header('Range', None)
+    request_range = REQUEST.get_header('Request-Range', None)
+    if request_range is not None:
+        # Netscape 2 through 4 and MSIE 3 implement a draft version
+        # Later on, we need to serve a different mime-type as well.
+        range = request_range
+    if_range = REQUEST.get_header('If-Range', None)
+    if range is not None:
+        ranges = parseRange(range)
+        if if_range is not None:
+            # Only send ranges if the data isn't modified, otherwise send
+            # the whole object. Support both ETags and Last-Modified dates!
+            if len(if_range) > 1 and if_range[:2] == 'ts':
+                # ETag:
+                if if_range != instance.http__etag():
+                    # Modified, so send a normal response. We delete
+                    # the ranges, which causes us to skip to the 200
+                    # response.
+                    ranges = None
+            else:
+                # Date
+                date = if_range.split(';')[0]
+                try:
+                    mod_since = long(DateTime(date).timeTime())
+                except:
+                    mod_since = None
+                if mod_since is not None:
+                    if instance._p_mtime:
+                        last_mod = long(instance._p_mtime)
+                    else:
+                        last_mod = long(0)
+                    if last_mod > mod_since:
+                        # Modified, so send a normal response. We delete
+                        # the ranges, which causes us to skip to the 200
+                        # response.
+                        ranges = None
+            RESPONSE.setHeader('Accept-Ranges', 'bytes')
+        if ranges and len(ranges) == 1:
+            [(start, end)] = ranges
+            iterator = blob.getIterator(start=start, end=end)
+            size = end - start
+            RESPONSE.setHeader('Content-Length', size)
+            RESPONSE.setHeader(
+                'Content-Range',
+                'bytes %d-%d/%d' % (start, end - 1, len(iterator)))
+            RESPONSE.setStatus(206) # Partial content
+            return iterator
--- src/plone/app/blob/field.py
+++ src/plone/app/blob/field.py
@@ -1,7 +1,6 @@
 from os import fstat
 from zope.interface import implements
 from StringIO import StringIO
-from DateTime.DateTime import DateTime
 from Acquisition import Implicit, aq_base
 from AccessControl import ClassSecurityInfo
 from ComputedAttribute import ComputedAttribute
@@ -12,7 +11,6 @@ except ImportError:
     from Globals import InitializeClass
 from ZODB.blob import Blob
 from ZODB.POSException import POSKeyError
-from ZPublisher.HTTPRangeSupport import parseRange
 from persistent import Persistent
 from transaction import savepoint
 from webdav.common import rfc1123_date
@@ -27,6 +25,7 @@ from plone.i18n.normalizer.interfaces import IUserPreferredFileNameNormalizer
 from plone.app.blob.interfaces import IBlobbable, IWebDavUpload, IBlobField
 from plone.app.blob.interfaces import IBlobWrapper
 from plone.app.blob.iterators import BlobStreamIterator
+from plone.app.blob.download import handleIfModifiedSince, handleRequestRange
 from plone.app.blob.utils import getImageSize
 
 
@@ -257,7 +256,7 @@ class BlobField(ObjectField):
         RESPONSE.setHeader('Last-Modified', rfc1123_date(instance._p_mtime))
         RESPONSE.setHeader('Content-Type', self.getContentType(instance))
         RESPONSE.setHeader('Accept-Ranges', 'bytes')
-        if self._if_modified_since_request_handler(
+        if handleIfModifiedSince(
             instance, REQUEST, RESPONSE):
             return ''
         RESPONSE.setHeader("Content-Length", blob.get_size())
@@ -269,91 +268,11 @@ class BlobField(ObjectField):
                 disposition=disposition,
                 filename=filename)
             RESPONSE.setHeader("Content-disposition", header_value)
-        iterator = self._range_request_handler(
+        iterator = handleRequestRange(
             instance, blob, REQUEST, RESPONSE)
         if iterator is None:
             iterator = blob.getIterator()
         return iterator
-
-    def _if_modified_since_request_handler(
-        self, instance, REQUEST, RESPONSE):
-        # HTTP If-Modified-Since header handling: return True if
-        # we can handle this request by returning a 304 response
-        header = REQUEST.get_header('If-Modified-Since', None)
-        if header is not None:
-            header = header.split(';')[0]
-            # Some proxies seem to send invalid date strings for this
-            # header. If the date string is not valid, we ignore it
-            # rather than raise an error to be generally consistent
-            # with common servers such as Apache (which can usually
-            # understand the screwy date string as a lucky side effect
-            # of the way they parse it).
-            # This happens to be what RFC2616 tells us to do in the face of an
-            # invalid date.
-            try:
-                mod_since = long(DateTime(header).timeTime())
-            except:
-                mod_since = None
-            if mod_since is not None:
-                if instance._p_mtime:
-                    last_mod = long(instance._p_mtime)
-                else:
-                    last_mod = long(0)
-                if last_mod > 0 and last_mod <= mod_since:
-                    RESPONSE.setStatus(304)
-                    return True
-
-    def _range_request_handler(
-        self, instance, blob, REQUEST, RESPONSE):
-        # check if we have a range in the request
-        ranges = None
-        range = REQUEST.get_header('Range', None)
-        request_range = REQUEST.get_header('Request-Range', None)
-        if request_range is not None:
-            # Netscape 2 through 4 and MSIE 3 implement a draft version
-            # Later on, we need to serve a different mime-type as well.
-            range = request_range
-        if_range = REQUEST.get_header('If-Range', None)
-        if range is not None:
-            ranges = parseRange(range)
-            if if_range is not None:
-                # Only send ranges if the data isn't modified, otherwise send
-                # the whole object. Support both ETags and Last-Modified dates!
-                if len(if_range) > 1 and if_range[:2] == 'ts':
-                    # ETag:
-                    if if_range != instance.http__etag():
-                        # Modified, so send a normal response. We delete
-                        # the ranges, which causes us to skip to the 200
-                        # response.
-                        ranges = None
-                else:
-                    # Date
-                    date = if_range.split(';')[0]
-                    try:
-                        mod_since = long(DateTime(date).timeTime())
-                    except:
-                        mod_since = None
-                    if mod_since is not None:
-                        if instance._p_mtime:
-                            last_mod = long(instance._p_mtime)
-                        else:
-                            last_mod = long(0)
-                        if last_mod > mod_since:
-                            # Modified, so send a normal response. We delete
-                            # the ranges, which causes us to skip to the 200
-                            # response.
-                            ranges = None
-                RESPONSE.setHeader('Accept-Ranges', 'bytes')
-            if ranges and len(ranges) == 1:
-                [(start, end)] = ranges
-                iterator = blob.getIterator(start=start, end=end)
-                size = end - start
-                RESPONSE.setHeader('Content-Length', size)
-                RESPONSE.setHeader(
-                    'Content-Range',
-                    'bytes %d-%d/%d' % (start, end - 1, len(iterator)))
-                RESPONSE.setStatus(206) # Partial content
-                return iterator
 
     security.declarePublic('get_size')
     def get_size(self, instance):
