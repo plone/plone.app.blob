80f524c2eb48c8b171496ddbb4e33f28d91d31a9
Andreas Zeidler <witsch@plone.org>
2010-02-11 17:46:04 +0000

    update [http://localhost:8080/plone/@@content-size-statistics view] to analyse approximate content size grouped by type
    svn path=/plone.app.blob/trunk/; revision=33918

--- src/plone/app/blob/browser/size.py
+++ src/plone/app/blob/browser/size.py
@@ -1,6 +1,6 @@
 from Acquisition import aq_inner
 from Products.Five import BrowserView
-from time import strftime
+from Products.CMFCore.utils import getToolByName
 
 
 def bytesize(size):
@@ -17,27 +17,33 @@ def bytesize(size):
 
 class FileContentSizeView(BrowserView):
 
-    def mklog(self):
-        """ helper to prepend a time stamp to the output """
-        write = self.request.RESPONSE.write
-        def log(msg, timestamp=True):
-            if timestamp:
-                msg = strftime('%Y/%m/%d-%H:%M:%S ') + msg
-            write(msg)
-        return log
-
-    def __call__(self, portal_type=['File', 'Image']):
+    def __call__(self):
         context = aq_inner(self.context)
-        log = self.mklog()
-        log('determining total size for content of type %r\n' % portal_type)
-        log('please hang on, this might take a moment...\n')
-        count = 0
-        size = 0L
-        brains = context.portal_catalog(portal_type=portal_type)
-        for brain in brains:
-            count += 1
-            size += bytesize(brain.getObjSize)
-            if count % 1000 == 0:
-                log('%d/%d...\n' % (count, len(brains)))
-        log('%d objects for a total size of %.2f MB (%d bytes)\n' % (count,
-            float(size) / 1024**2, size))
+        query = dict(path='/'.join(context.getPhysicalPath()))
+        query.update(self.request.form)
+        if '-C' in query:
+            del query['-C']
+        write = self.request.RESPONSE.write
+        def log(msg, *args):
+            write(msg % args + '\n')
+        log('Analysing content size (parameters %r)', query)
+        log('Please hang on, this might take a moment...')
+        counts = {}
+        sizes = {}
+        catalog = getToolByName(context, 'portal_catalog')
+        brains = catalog.unrestrictedSearchResults(**query)
+        log('Found %d results, counting...', len(brains))
+        for idx, brain in enumerate(brains):
+            pt = brain.portal_type
+            counts[pt] = counts.setdefault(pt, 0) + 1
+            sizes[pt] = sizes.setdefault(pt, 0L) + bytesize(brain.getObjSize)
+            if idx % 1000 == 0:
+                log('%d/%d...', idx, len(brains))
+        log('')
+        fmt = '%-20s %6s items, %6.1f mb (%d bytes),\t%12.1f avg'
+        for pt, count in counts.items():
+            size = float(sizes[pt])
+            log(fmt, pt + ':', count, size / 1024**2, size, size / count)
+        count = sum(counts.values())
+        size = float(sum(sizes.values()))
+        log(fmt, 'Totals', count, size / 1024**2, size, size / count)
