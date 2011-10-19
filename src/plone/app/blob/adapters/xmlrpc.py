40f0a3ec271c6950901fc72597f2c2c9c8a3407b
Alex Clark <aclark@aclark.net>
2011-10-19 13:39:05 -0400

    Clean up

--- src/plone/app/blob/adapters/xmlrpc.py
+++ src/plone/app/blob/adapters/xmlrpc.py
@@ -4,7 +4,6 @@ from zope.component import adapts
 from xmlrpclib import Binary
 
 from plone.app.blob.interfaces import IBlobbable
-#from plone.app.blob.utils import guessMimetype
 
 
 class BlobbableBinary(object):
@@ -17,12 +16,9 @@ class BlobbableBinary(object):
 
     def feed(self, blob):
         """ see interface ... """
-#        pos = self.context.tell()
-#        self.context.seek(0)
         blobfile = blob.open('w')
         blobfile.writelines(self.context.data)
         blobfile.close()
-#        self.context.seek(pos)
 
     def filename(self):
         """ see interface ... """
@@ -30,6 +26,4 @@ class BlobbableBinary(object):
 
     def mimetype(self):
         """ see interface ... """
-#        return guessMimetype(self.context, self.filename())
         return 'binary'
-
