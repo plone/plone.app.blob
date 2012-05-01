c819b347b4fe265a2472ec28659b0a863138bb80
Luca Fabbri <luca@keul.it>
2012-05-01 23:03:39 +0200

    a working support for blobbed news

--- src/plone/app/blob/markings.py
+++ src/plone/app/blob/markings.py
@@ -4,23 +4,26 @@
 from zope.interface import alsoProvides, noLongerProvides
 from Products.ATContentTypes.interface import file as atfile
 from Products.ATContentTypes.interface import image as atimage
-from plone.app.blob.interfaces import IATBlobBlob, IATBlobFile, IATBlobImage
+from Products.ATContentTypes.interface import news as atnews
+from plone.app.blob.interfaces import IATBlobBlob, IATBlobFile, IATBlobImage, IATBlobNewsItem
 
 # support for zope2-interfaces
 from Products.ATContentTypes.interfaces import IATFile as Z2IATFile
 from Products.ATContentTypes.interfaces import IATImage as Z2IATImage
-
+from Products.ATContentTypes.interfaces import IATNewsItem as Z2IATNewsItem
 
 interfaces = {
     'Blob': [IATBlobBlob, atfile.IATFile, atfile.IFileContent],
     'File': [IATBlobFile, atfile.IATFile, atfile.IFileContent],
     'Image': [IATBlobImage, atimage.IATImage, atimage.IImageContent],
+    'News Item': [IATBlobNewsItem, atnews.IATNewsItem],
 }
 
 z2interfaces = {
     'Blob': [Z2IATFile],
     'File': [Z2IATFile],
     'Image': [Z2IATImage],
+    'News Item': [Z2IATNewsItem],
 }
 
 
