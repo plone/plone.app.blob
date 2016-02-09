778b4cae6c7dc8e56512b1c1f94177867cae9e07
Maurits van Rees <maurits@vanrees.org>
2016-02-09 23:12:12 +0100

    Used https in pypi url.
    [ci skip]

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     author='Plone Foundation',
     author_email='plone-developers@lists.sourceforge.net',
     url='http://plone.org/products/plone.app.blob',
-    download_url='http://pypi.python.org/pypi/plone.app.blob/',
+    download_url='https://pypi.python.org/pypi/plone.app.blob/',
     license='GPL version 2',
     packages=find_packages('src'),
     package_dir={'': 'src'},
