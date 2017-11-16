plone/plone.app.blob

commit a0fb3b38b68a54aae9cb448acdb85983a087c2cb

Blob images should also reset EXIF data on save (#26)
* Update exif data on save
* refresh_exif should default to True

src/plone/app/blob/subtypes/image.py
@@ -17,9 +17,10 @@ class ExtensionBlobField(ExtensionField, BlobField, ImageFieldMixin):
     """ derivative of blobfield for extending schemas """
     implements(IBlobImageField)
 
-    def set(self, instance, value, **kwargs):
+    def set(self, instance, value, refresh_exif=True, **kwargs):
         super(ExtensionBlobField, self).set(instance, value, **kwargs)
         self.fixAutoId(instance)
+        instance.getEXIF(value, refresh=refresh_exif)
         if hasattr(aq_base(instance), blobScalesAttr):
             delattr(aq_base(instance), blobScalesAttr)
 

commit e54618afcb6c05bb0ba0a9235d58c9c8f557fe6f

Fix bug introduced by #26 that prevents image blob types being used by content types that did not originally have an image field.
This is done by making the fix in #26 selective, rather than porting
the EXIF functionality from ATContentTypes to plone.app.blob. That
is a valid feature, but presumably low priority.

src/plone/app/blob/subtypes/image.py
@@ -21,7 +21,11 @@ class ExtensionBlobField(ExtensionField, BlobField, ImageFieldMixin):
     def set(self, instance, value, refresh_exif=True, **kwargs):
         super(ExtensionBlobField, self).set(instance, value, **kwargs)
         self.fixAutoId(instance)
-        instance.getEXIF(value, refresh=refresh_exif)
+        if hasattr(instance, 'getEXIF'):
+            # If the instance subclasses ATCTImageTransform we process the
+            # metadata.
+            # TODO: The EXIF functionality should not be dependent on ATCT
+            instance.getEXIF(value, refresh=refresh_exif)
         if hasattr(aq_base(instance), blobScalesAttr):
             delattr(aq_base(instance), blobScalesAttr)
 

src/plone/app/blob/tests/base.py
@@ -2,6 +2,7 @@
 from plone.app.blob.tests.layer import BlobLayer
 from plone.app.blob.tests.layer import BlobLinguaLayer
 from plone.app.blob.tests.layer import BlobReplacementLayer
+from plone.app.blob.tests.layer import BlobSchemaExtenderLayer
 from plone.app.testing import TEST_USER_NAME
 from plone.app.testing import TEST_USER_PASSWORD
 from plone.app.testing.bbb import PloneTestCase
@@ -35,6 +36,12 @@ class ReplacementTestCase(BlobTestCase):
     layer = BlobReplacementLayer
 
 ReplacementFunctionalTestCase = ReplacementTestCase
+
+
+class BlobSchemaExtenderTestCase(PloneTestCase):
+    """ base class for schemaextender tests """
+
+    layer = BlobSchemaExtenderLayer
 
 
 class BlobLinguaTestCase(PloneTestCase):

src/plone/app/blob/tests/extender.py
@@ -1,0 +1,36 @@
+from zope.component import adapts
+from zope.interface import implements
+from archetypes.schemaextender.interfaces import ISchemaExtender
+from Products.ATContentTypes.interfaces import IATDocument, IATImage
+
+from plone.app.blob.subtypes.image import ExtensionBlobField
+
+
+class PageImageAdder(object):
+    adapts(IATDocument)
+    implements(ISchemaExtender)
+
+    fields = [
+        ExtensionBlobField("image"),
+    ]
+
+    def __init__(self, context):
+        self.context = context
+
+    def getFields(self):
+        return self.fields
+
+
+class ImageImageAdder(object):
+    adapts(IATImage)
+    implements(ISchemaExtender)
+
+    fields = [
+        ExtensionBlobField("new_image"),
+    ]
+
+    def __init__(self, context):
+        self.context = context
+
+    def getFields(self):
+        return self.fields

src/plone/app/blob/tests/extender.zcml
@@ -1,0 +1,12 @@
+<configure
+    xmlns="http://namespaces.zope.org/zope"
+    xmlns:zcml="http://namespaces.zope.org/zcml"
+    xmlns:five="http://namespaces.zope.org/five"
+    xmlns:monkey="http://namespaces.plone.org/monkey"
+    i18n_domain="plone.app.blob">
+
+    <include package="archetypes.schemaextender" />
+    <adapter factory=".extender.PageImageAdder" />
+    <adapter factory=".extender.ImageImageAdder" />
+
+</configure>

src/plone/app/blob/tests/layer.py
@@ -81,6 +81,32 @@ BlobReplacementLayer = testing.FunctionalTesting(
 BlobFileReplacementLayer = BlobReplacementLayer
 
 
+class BlobSchemaExtenderFixture(PloneTestCaseFixture):
+    """ layer for integration tests with SchemaExtender """
+
+    defaultBases = (PTC_FIXTURE, )
+
+    def setUpZope(self, app, configurationContext):
+        import archetypes.schemaextender
+        self.loadZCML(package=archetypes.schemaextender)
+        import plone.app.blob
+        self.loadZCML(package=plone.app.blob)
+        self.loadZCML(name='extender.zcml', package=plone.app.blob.tests)
+        z2.installProduct(app, 'plone.app.blob')
+        z2.installProduct(app, 'archetypes.schemaextender')
+
+    def tearDownZope(self, app):
+        z2.uninstallProduct(app, 'plone.app.blob')
+        z2.uninstallProduct(app, 'archetypes.schemaextender')
+
+
+BLOB_SCHEMA_EXTENDER_FIXTURE = BlobSchemaExtenderFixture()
+BlobSchemaExtenderLayer = testing.FunctionalTesting(
+    bases=(BLOB_SCHEMA_EXTENDER_FIXTURE, ),
+    name='Blob SchemaExtender:Functional',
+)
+
+
 class BlobLinguaFixture(PloneTestCaseFixture):
     """ layer for integration tests with LinguaPlone """
 

src/plone/app/blob/tests/test_extensionblobfield.py
@@ -1,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from unittest import defaultTestLoader
+from unittest import TestSuite
+
+from plone.app.blob.tests.base import BlobSchemaExtenderTestCase
+from plone.app.blob.tests.utils import hasSchemaExtender
+
+
+class ExtenderTests(BlobSchemaExtenderTestCase):
+
+    def testImageOnDocument(self):
+        """Test that adding an image blob field to a document doesn't
+        error for lack of EXIF helper functions"""
+        document_id = self.folder.invokeFactory('Document', id='doc')
+        document = self.folder[document_id]
+        document.Schema().get('image').set(document, 'f')
+
+    def testImageOnImage(self):
+        """Test that an extension image field works on a class that has image
+        helper methods"""
+        img_id = self.folder.invokeFactory('Image', id='img')
+        img = self.folder[img_id]
+        img.Schema().get('new_image').set(img, 'f')
+
+
+def test_suite():
+    if hasSchemaExtender():
+        return defaultTestLoader.loadTestsFromName(__name__)
+    else:
+        return TestSuite()

src/plone/app/blob/tests/utils.py
@@ -60,3 +60,17 @@ def hasLinguaPlone():
         print msg
         print '*' * len(msg)
         return False
+
+
+def hasSchemaExtender():
+    """ test if SchemaExtender is available """
+    try:
+        import archetypes.schemaextender
+        archetypes.schemaextender     # make pyflakes happy...
+        return True
+    except ImportError:
+        msg = 'WARNING: archetypes.schemaextender not found. Skipping tests.'
+        print '*' * len(msg)
+        print msg
+        print '*' * len(msg)
+        return False