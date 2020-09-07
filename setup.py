d2b9d3f96415ef5fd8fa75519c0ce2906de09eab
Maurits van Rees <maurits@vanrees.org>
2020-09-07 22:30:02 +0200

    Added dev status mature classifier.
    [ci skip]

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ setup(
     tests_require=tests_require,
     extras_require={'test': tests_require},
     classifiers=[
+        'Development Status :: 6 - Mature',
         'Environment :: Web Environment',
         'Framework :: Plone :: 5.0',
         'Framework :: Plone :: 5.1',
