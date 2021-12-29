22e8cf98981c0d480ff763f6bac94dddbe84fc17
Maurits van Rees <maurits@vanrees.org>
2021-12-30 00:11:04 +0100

    Back to development: 1.8.3
    [ci skip]

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 
-version = '1.8.2'
+version = '1.8.3.dev0'
 
 long_description = (
     read('README.rst')
